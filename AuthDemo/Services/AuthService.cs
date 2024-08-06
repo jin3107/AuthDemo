50cd51b6fed716d167be8f4b3780a4515d52a483
Huỳnh Tấn Chương <tanchuonghuynh3@gmail.com>
2024-08-06 20:32:17 +0700

    06/08/2024

--- AuthDemo/Services/AuthService.cs
+++ AuthDemo/Services/AuthService.cs
@@ -66,7 +66,7 @@ namespace AuthDemo.Services
                     ExpiresIn = token.ValidTo
                 };
             }
-            return null;
+            return null; 
         }
     }
 }
